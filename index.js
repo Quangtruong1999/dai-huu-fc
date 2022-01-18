e9c794bd4f275a7de72543b37d50b0f9148abeec
hideonbush0512@gmail.com <hideonbush0512@gmail.com>
2022-01-18 17:20:01 +0700

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+// const path = require('path')
+const express = require('express')
+// const morgan = require('morgan')
+// const { engine } = require('express-handlebars');
+// const { render } = require('express/lib/response')
+const app = express()
+const port = 3000
+
+
+// app.use(express.static(path.join(__dirname,'public')))
+
+//HTTP logger
+// app.use(morgan('combined'))
+
+//Templates engine
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// console.log('path = ',path.join(__dirname,'resources/views') )
+// app.set('views', path.join(__dirname,'resources/views'));
+
+
+//
+// app.get('/', (req, res) => {
+//   res.render('home')})
+app.get('/', (req, res) => {res.send('Hello')})
+  
+// app.get('/news', (req, res) => {
+//   res.render('news')})
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
