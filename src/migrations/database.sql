0ef934a7884f3122beaf65ef2beb8c2cbb38ca03
hideonbush0512@gmail.com <hideonbush0512@gmail.com>
2022-03-19 20:09:15 +0700

    create database

--- src/migrations/database.sql
+++ src/migrations/database.sql
@@ -11,26 +11,48 @@ CREATE TABLE IF NOT EXISTS contact(
     email VARCHAR(255),
     content TEXT,
     CONSTRAINT A_CONTACT_FK PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE IF NOT EXISTS manager(
     ID SERIAL,
     full_name VARCHAR(255),
     nationality VARCHAR(255),
-    current_team int
-)
+    current_team INT4,
+    position_id INT4,
+    CONSTRAINT A_MANAGER_PK PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS partners(
+    ID SERIAL,
+    full_name VARCHAR(255),
+    link_web VARCHAR(255),
+    content INT4,
+    CONSTRAINT A_PARTNERS_PK PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS banners(
+    ID SERIAL,
+    link VARCHAR(255),
+    quote VARCHAR(255),
+    CONSTRAINT A_BANNERS_PK PRIMARY KEY (ID)
+);
+
+
+ALTER TABLE manager ADD CONSTRAINT FK_MANAGER_POSITION FOREIGN KEY (current_team) REFERENCES position (ID);
+
+
 
 
 
 -- SET SEARCH_PATH TO work;
 --Insert into position table
-INSERT INTO position (name) VALUES ('Thủ môn');
-INSERT INTO position (name) VALUES ('Tiền vệ');
-INSERT INTO position (name) VALUES ('Tiền đạo');
-INSERT INTO position (name) VALUES ('HLV Trưởng');
-INSERT INTO position (name) VALUES ('Trợ lý HLV');
-INSERT INTO position (name) VALUES ('Chuyên viên y tế');
-INSERT INTO position (name) VALUES ('Chuyên viên vật lý trị liệu');
-INSERT INTO position (name) VALUES ('Cán bộ hậu cần');
+-- INSERT INTO position (name) VALUES ('Thủ môn');
+-- INSERT INTO position (name) VALUES ('Tiền vệ');
+-- INSERT INTO position (name) VALUES ('Tiền đạo');
+-- INSERT INTO position (name) VALUES ('HLV Trưởng');
+-- INSERT INTO position (name) VALUES ('Trợ lý HLV');
+-- INSERT INTO position (name) VALUES ('Chuyên viên y tế');
+-- INSERT INTO position (name) VALUES ('Chuyên viên vật lý trị liệu');
+-- INSERT INTO position (name) VALUES ('Cán bộ hậu cần');
 
 -- drop table position;
--- src/routes/teams.js
+++ src/routes/teams.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const team_controller = require('../app/controllers/TeamController')
+var axios = require('axios');
+
+var request = require("request");
+
+var options = {
+  method: 'GET',
+  url: 'https://v3.football.api-sports.io/teams',
+  qs: {country: 'Vietnam'},
+  headers: {
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': '8c917127dbeaff2e76525aec279df071'
+  }
+};
+
+// request(options, function (error, response, body) {
+// 	if (error) throw new Error(error);
+
+// 	console.log(body);
+// });
+
 
 router.use('/', team_controller.index)
 
